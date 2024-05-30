70346b8cf3ae1eb9053d9c8ecf7bec0783904dfa
SoftDev-Abay <abay.aliev.03@gmail.com>
2024-05-30 12:01:17 +0500

    asd

--- app/src/pages/home/Notifications.jsx
+++ app/src/pages/home/Notifications.jsx
@@ -1,87 +1,74 @@
-import React from "react";
+import React from 'react';
 
-import NotificationCard from "../../components/NotificationCard";
-import Spinner from "../../components/Spinner";
-
+import NotificationCard from '../../components/NotificationCard';
+import Spinner from '../../components/Spinner';
+import { useBlog } from '../../context/Blog';
 // mock data
 const data = {
-  visitors: [
-    {
-      _id: "1",
-      visitorId: {
-        _id: "1",
-        name: "John Doe",
-        profilePicture: "",
-      },
-      visitedAt: new Date(),
-    },
-    {
-      _id: "2",
-      visitorId: {
-        _id: "2",
-        name: "Jane Doe",
-        profilePicture: "",
-      },
-      visitedAt: new Date(),
-    },
-  ],
+    visitors: [
+        {
+            _id: '1',
+            visitorId: {
+                _id: '1',
+                name: 'John Doe',
+                profilePicture: '',
+            },
+            visitedAt: new Date(),
+        },
+    ],
 };
 
 function Notifications() {
-  //   const { data, error, isLoading, isSuccess } = useGetVisitorsQuery();
+    const { user, request, userAccounts, acceptRequest } = useBlog();
 
-  // statics
-  const isLoading = false;
-  const error = null;
-  const isSuccess = true;
+    let content;
 
-  let content;
+    const onSubmit = (key) => {
+        acceptRequest(key);
+    };
 
-  if (isLoading) {
-    content = (
-      <div className="flex justify-center items-center mt-20">
-        <Spinner />
-      </div>
+    console.log(request);
+
+    const userRequests = request.reduce((acc, req) => {
+        const currentUser = userAccounts.find(
+            (user) => user.authority.PublicKey.toBase58() === req.PublicKey
+        );
+
+        return currentUser;
+    }, []);
+
+    // if (userRequests.length === 0) {
+    //     content = (
+    //         <div className="flex justify-center items-center mt-20">
+    //             <p className="text-center text-gray-1">No notifications</p>
+    //         </div>
+    //     );
+    // } else {
+    //     content = (
+    //         <div className="mt-14 flex flex-col gap-9">
+    //             {userRequests.map((visitor, index) => (
+    //                 <NotificationCard
+    //                     key={
+    //                         index +
+    //                         visitor.account.name +
+    //                         visitor.account.avatar
+    //                     }
+    //                     message={`${visitor.account.name} send you request`}
+    //                     userImage={visitor.account.avatar}
+    //                     publicKey={visitor.PublicKey}
+    //                     onSubmit={onSubmit}
+    //                 />
+    //             ))}
+    //         </div>
+    //     );
+    // }
+
+    return (
+        <section>
+            <h1 className="head-text mb-10">Activity</h1>
+            {content}
+        </section>
     );
-  }
-
-  if (error) {
-    content = <div>Error: {error.message}</div>;
-  }
-
-  console.log(data);
-
-  if (isSuccess) {
-    if (data.visitors.length === 0) {
-      content = (
-        <div className="flex justify-center items-center mt-20">
-          <p className="text-center text-gray-1">No notifications</p>
-        </div>
-      );
-    } else {
-      content = (
-        <div className="mt-14 flex flex-col gap-9">
-          {data.visitors.map((visitor) => (
-            <NotificationCard
-              key={visitor._id}
-              id={visitor.visitorId._id}
-              message={`${visitor.visitorId.name} visited your profile`}
-              date={visitor.visitedAt}
-              userImage={visitor.visitorId.profilePicture}
-              href={`/user-profile/${visitor.visitorId._id}`}
-            />
-          ))}
-        </div>
-      );
-    }
-  }
-
-  return (
-    <section>
-      <h1 className="head-text mb-10">Activity</h1>
-      {content}
-    </section>
-  );
 }
 
 export default Notifications;
--- app/src/shared/layout/Topbar.jsx
+++ app/src/shared/layout/Topbar.jsx
@@ -1,113 +1,55 @@
-import React, { useState, useEffect } from "react";
-import { Link } from "react-router-dom";
-import { useWallet } from "@solana/wallet-adapter-react";
-import { PhantomWalletName } from "@solana/wallet-adapter-wallets";
-import { Button } from "src/components/Button";
-import { useBlog } from "src/context/Blog";
-import { useHistory } from "react-router-dom";
+import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
+
+import { useBlog } from 'src/context/Blog';
+import { useHistory } from 'react-router-dom';
 
 function Topbar() {
-  const history = useHistory();
-  const [connecting, setConnecting] = useState(false);
-  const { connected, select } = useWallet();
-  const { user, initialized, initUser, setShowModal } = useBlog();
-  const [postTitle, setPostTitle] = useState("");
-  const [postContent, setPostContent] = useState("");
+    const history = useHistory();
 
-  const onConnect = () => {
-    setConnecting(true);
-    select(PhantomWalletName);
-  };
+    const { user } = useBlog();
 
-  useEffect(() => {
-    if (user) {
-      setConnecting(false);
-    }
-  }, [user]);
+    return (
+        <nav className="topbar">
+            <Link to="/" className="flex items-center gap-4">
+                <img src="/assets/logo.svg" alt="logo" width={28} height={28} />
+                <p className="text-heading3-bold text-light-1 max-xs:hidden">
+                    Threads
+                </p>
+            </Link>
 
-  return (
-    <nav className="topbar">
-      <Link to="/" className="flex items-center gap-4">
-        <img src="/assets/logo.svg" alt="logo" width={28} height={28} />
-        <p className="text-heading3-bold text-light-1 max-xs:hidden">Threads</p>
-      </Link>
-
-      <div className="flex gap-2">
-        <div className="flex items-center gap-1">
-          <div className="block">
-            <div className="flex cursor-pointer">
-              {connected ? (
-                <div className="flex items-center">
-                  <p className="font-bold text-sm ml-2 capitalize underlinepink">
-                    Home
-                  </p>
-                  <p className="font-bold text-sm ml-2 capitalize mr-4 underlinepink">
-                    Blog
-                  </p>
-                  <img
-                    src={user?.avatar}
-                    alt="avatar"
-                    className="w-8 h-8 rounded-full bg-gray-200 shadow ring-2 ring-indigo-400 ring-offset-2 ring-opacity-50"
-                  />
-                  <p className="font-bold text-sm ml-2 capitalize">
-                    {user?.name}
-                  </p>
-                  {initialized ? (
-                    <span>Logged in</span>
-                  ) : (
-                    <Button
-                      className="ml-3 mr-2"
-                      onClick={() => {
-                        initUser();
-                      }}
-                    >
-                      Initialize User
-                    </Button>
-                  )}
+            <div className="flex gap-2">
+                <div className="flex items-center gap-1">
+                    <div className="block">
+                        <div className="flex cursor-pointer">
+                            <div className="flex items-center">
+                                <img
+                                    src={user?.avatar}
+                                    alt="avatar"
+                                    className="w-8 h-8 rounded-full bg-gray-200 shadow ring-2 ring-indigo-400 ring-offset-2 ring-opacity-50"
+                                />
+                                <p className="font-bold text-sm ml-2 capitalize">
+                                    {user?.name}
+                                </p>
+                            </div>
+                        </div>
+                    </div>
                 </div>
-              ) : (
-                <Button
-                  loading={connecting}
-                  className="w-28"
-                  onClick={onConnect}
-                  leftIcon={
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      className="h-5 w-5 mr-1"
-                      fill="none"
-                      viewBox="0 0 24 24"
-                      stroke="currentColor"
-                    >
-                      <path
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                        strokeWidth={2}
-                        d="M9 12l2 2 4-4m5.618-4.016A11.955 11.955 0 0112 2.944a11.955 11.955 0 01-8.618 3.04A12.02 12.02 0 003 9c0 5.591 3.824 10.29 9 11.622 5.176-1.332 9-6.03 9-11.622 0-1.042-.133-2.052-.382-3.016z"
-                      />
-                    </svg>
-                  }
-                >
-                  Connect
-                </Button>
-              )}
+                <div className="flex items-center gap-1">
+                    <div className="block md:hidden">
+                        <div className="flex cursor-pointer" onClick={() => {}}>
+                            <img
+                                src="/assets/logout.svg"
+                                alt="logout"
+                                width={24}
+                                height={24}
+                            />
+                        </div>
+                    </div>
+                </div>
             </div>
-          </div>
-        </div>
-        <div className="flex items-center gap-1">
-          <div className="block md:hidden">
-            <div className="flex cursor-pointer" onClick={() => {}}>
-              <img
-                src="/assets/logout.svg"
-                alt="logout"
-                width={24}
-                height={24}
-              />
-            </div>
-          </div>
-        </div>
-      </div>
-    </nav>
-  );
+        </nav>
+    );
 }
 
 export default Topbar;
